9d9ee9d7d6353879ca69f8e43f7c6408f071b209
Saiprasad V R <78998314+Saiprasad-V-R@users.noreply.github.com>
2025-06-26 18:21:33 +0530

    Update Lyupunav.f

--- Lyupunav.f
+++ Lyupunav.f
@@ -1,269 +1,260 @@
-C	*********************************************************************
-C	PROGRAM FOR CALCULATING LYAPUNOV EXPONENT FOR LIENARD OSCILLATOR
-C	*********************************************************************
+      PROGRAM Eco-Epidemic
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      INTEGER N, I, K, L, NPAS, NRUN, NT, NLYA, IT, NN
+      DOUBLE PRECISION X(100), CUM(100), ZNORM(100), GSC(100)
+      DOUBLE PRECISION H, T
 
-	IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-	DIMENSION X(100),CUM(100),ZNORM(100),GSC(100)
+      COMMON /C1/ ar, ak, abeta, abet0, amu, ags, agi, d, T, eps, beta
+      COMMON /C3/ N
 
-	 COMMON/C1/ar,ak,abeta,abet0,amu,ags,agi,d,T,eps,beta
-	 COMMON/C3/N
-	
-      open(14,file='test_avg_ly.dat')
+      ! Open output file for average Lyapunov exponents
+      OPEN(14, FILE='test_avg_ly.dat')
 
+      ! Model parameters
+      ar    = 2.0D0
+      ak    = 1000.0D0
+      abeta = 6.90D0
+      amu   = 0.001D0
+      ags   = 9.10D0
+      agi   = 1.0D0
+      eps   = 0.2D0
+      d     = 0.1D0
+      T     = 0.0D0
+      N     = 3
+      NN    = N*N + N        ! Total dimension for state + variational
 
-       
-		
-      ar = 2.0d0
-      ak = 1000.0d0
-      abeta = 6.90d0
-      amu = 0.001d0
-      ags = 9.10d0
-      agi = 1.0d0
-      eps = 0.2d0
-      d = 0.1d0
-      T = 0.0D0
-      N=3
-      NN=(N*N)+N
-
+      ! Simulation settings
       NPAS = 2000
       NRUN = 500
-      NT = 500
-      NLYA = 1000*NRUN
-      IT = NT*NPAS
+      NT   = 500
+      NLYA = 1000 * NRUN     ! Number of steps for Lyapunov calc
+      IT   = NT * NPAS       ! Transient spin-up steps
+      H    = 0.01D0          ! Time step
 
-	H = 0.01D0
- !      call random_number(x(1))
-!      X(1) = sqrt(-2*log(u1))*cos(2*pi*u2)
-      	X(1) = 0.051101d0
-	X(2) = 0.0204d0
-	X(3) = 0.3040d0
+      ! Initial conditions for main system
+      X(1) = 0.051101D0
+      X(2) = 0.0204D0
+      X(3) = 0.3040D0
 
-!       write (*,12)xi,x(1)
-!12     format(2F9.3)
-      DO I = 1,IT
-        CALL RK1(X,H)
-      END DO
-C
-C     INITIAL CONDITION FOR LINEAR SYSTEM
-
-    	 X(4) = 0.051100d0
-	X(5) = 0.0204d0
-	X(6) = 0.3040d0
-	X(7) = 0.051100d0
-	X(8) = 0.0204d0
-	X(9) = 0.3040d0
-	X(10) = 0.051100d0
-  
-      DO I=1,N
-      X((N+1)*I)=1.0D0
-      CUM(I)=0.0D0
-      END DO
-c
-      do abet0 =0.0d0,3.0d0,0.005d0
-      DO I = 1,NLYA
-        t=t+h
-      CALL RK4(X,H)
-c
-C     NORMALIZE FIRST VECTOR
-
-      ZNORM(1)=0.0D0
-      DO J=1,N
-      ZNORM(1)=ZNORM(1)+X(N*J+1)**2
-      END DO
-      ZNORM(1)=SQRT(ZNORM(1))
-      DO J=1,N
-      X(N*J+1)=X(N*J+1)/ZNORM(1)
-      END DO
-C
-C     GENERATE THE NEW ORTHONORMAL SET OF VECTORS
-      DO 40 J=2,N
-C
-C     GENERATE J-1 GSR COEFFICIENTS
-      DO 10 K=1,J-1
-      GSC(K)=0.0D0
-      DO 10 L=1,N
-      GSC(K)=GSC(K)+X(N*L+J)*X(N*L+K)
-10    CONTINUE
-C
-C     CONSTRUCT A NEW VECTOR
-      DO 20 K=1,N
-      DO 20 L=1,J-1
-      X(N*K+J)=X(N*K+J)-GSC(L)*X(N*K+L)
-20    CONTINUE
-C
-C     CALCULATE THE VECTOR'S NORM
-      ZNORM(J)=0.0D0
-      DO 30 K=1,N
-      ZNORM(J)=ZNORM(J)+X(N*K+J)**2
-30    CONTINUE
-      ZNORM(J)=SQRT(ZNORM(J))
-C
-C     NORMALIZE THE NEW VECTOR
-      DO 40 K=1,N
-      X(N*K+J)=X(N*K+J)/ZNORM(J)
-40    CONTINUE
-                                                                                
-C     UPDATE RUNNING VECTOR MAGNITUDES
-      DO K=1,N
-      CUM(K)=CUM(K)+DLOG(ZNORM(K))/DLOG(2.0D0)
-      END DO
+      ! --- Remove transients via simple RK stepping ---
+      DO I = 1, IT
+          CALL RK1(X, H)
       END DO
 
-      DO K=1,N
-      CUM(K)=CUM(K)/(FLOAT(NLYA)*H)
+      ! Initialize variational vectors and cumulants
+      DO I = 1, N
+          X(N+I) = X(I)       ! Copy state into first perturbation block
+          CUM(I)   = 0.0D0    ! Reset cumulant sums
       END DO
-C
-       
-       
-15	format(1x,4f10.6)
-      WRITE(14,*)abet0,CUM(1),CUM(2),CUM(3)
-C      WRITE(*,*)aep_v,CUM(1),CUM(2),CUM(3)
-      print 15,abet0,CUM(1),CUM(2),CUM(3)
+
+      ! --- Loop over forcing amplitude abet0 ---
+      DO abet0 = 0.0D0, 3.0D0, 0.005D0
+
+          T = 0.0D0          ! Reset time for this run
+
+          ! Reset perturbation vectors to identity basis
+          DO I = 1, N
+              DO K = 1, N
+                  IF (K .EQ. I) THEN
+                      X(N*K + I) = 1.0D0
+                  ELSE
+                      X(N*K + I) = 0.0D0
+                  END IF
+              END DO
+              CUM(I) = 0.0D0
+          END DO
+
+          ! --- Main RK4 integration for Lyapunov exponents ---
+          DO I = 1, NLYA
+              T = T + H
+              CALL RK4(X, H)
+
+              ! Orthonormalize perturbation vectors (Gram–Schmidt)
+              ! First vector
+              ZNORM(1) = 0.0D0
+              DO K = 1, N
+                  ZNORM(1) = ZNORM(1) + X(N*K+1)**2
+              END DO
+              ZNORM(1) = SQRT(ZNORM(1))
+              DO K = 1, N
+                  X(N*K+1) = X(N*K+1) / ZNORM(1)
+              END DO
+
+              ! Remaining vectors
+              DO J = 2, N
+                  ! Compute projections onto earlier vectors
+                  DO K = 1, J-1
+                      GSC(K) = 0.0D0
+                      DO L = 1, N
+                          GSC(K) = GSC(K) + X(N*L+J)*X(N*L+K)
+                      END DO
+                  END DO
+
+                  ! Subtract projections
+                  DO K = 1, N
+                      DO L = 1, J-1
+                          X(N*K+J) = X(N*K+J) - GSC(L)*X(N*K+L)
+                      END DO
+                  END DO
+
+                  ! Normalize J-th vector
+                  ZNORM(J) = 0.0D0
+                  DO K = 1, N
+                      ZNORM(J) = ZNORM(J) + X(N*K+J)**2
+                  END DO
+                  ZNORM(J) = SQRT(ZNORM(J))
+                  DO K = 1, N
+                      X(N*K+J) = X(N*K+J) / ZNORM(J)
+                  END DO
+              END DO
+
+              ! Accumulate log norms for exponents
+              DO K = 1, N
+                  CUM(K) = CUM(K) + LOG(ZNORM(K)) / LOG(2.0D0)
+              END DO
+          END DO
+
+          ! Compute average Lyapunov exponents
+          DO K = 1, N
+              CUM(K) = CUM(K) / (NLYA * H)
+          END DO
+
+          ! Output for current abet0
+          WRITE(14,*) abet0, CUM(1), CUM(2), CUM(3)
+          PRINT *,  abet0, CUM(1), CUM(2), CUM(3)
       END DO
-	STOP
-      END
-C	********************************************************************
-      SUBROUTINE RK4(X,H)
+
+      STOP
+      END PROGRAM LienardLyapunov
+
+
+      SUBROUTINE RK4(X, H)
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      DIMENSION X(100),TEMP(100),AK1(100),AK2(100),AK3(100)
-      DIMENSION AK4(100),PRIME(100)
+      INTEGER I, NN
+      DOUBLE PRECISION X(100), TEMP(100), AK1(100), AK2(100), AK3(100), AK4(100), PRIME(100)
+      COMMON /C3/ N
 
-      COMMON/C3/N
+      NN = N*N + N     ! Total system dimension
 
-      NN=(N*N)+N
-
-      DO I=1,NN
-      TEMP(I)=X(I)
+      ! Copy state to TEMP
+      DO I = 1, NN
+          TEMP(I) = X(I)
       END DO
 
-      CALL DERIVE(TEMP,PRIME)
-      DO I=1,NN
-      AK1(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK1(I)/2.0D0
+      ! Fourth‐order Runge–Kutta steps
+      CALL DERIVE(TEMP, PRIME)
+      DO I = 1, NN
+          AK1(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK1(I)/2.0D0
       END DO
 
-      CALL DERIVE(TEMP,PRIME)
-      DO I=1,NN
-      AK2(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK2(I)/2.0D0
+      CALL DERIVE(TEMP, PRIME)
+      DO I = 1, NN
+          AK2(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK2(I)/2.0D0
       END DO
 
-      CALL DERIVE(TEMP,PRIME)
-      DO I=1,NN
-      AK3(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK3(I)
+      CALL DERIVE(TEMP, PRIME)
+      DO I = 1, NN
+          AK3(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK3(I)
       END DO
 
-      CALL DERIVE(TEMP,PRIME)
-      DO I=1,NN
-      AK4(I)=H*PRIME(I)
-      X(I)=X(I)+1/6.0D0*(AK1(I)+2.0D0*(AK2(I)+AK3(I))+AK4(I))
+      CALL DERIVE(TEMP, PRIME)
+      DO I = 1, NN
+          AK4(I) = H*PRIME(I)
+          X(I) = X(I) + (AK1(I) + 2.0D0*(AK2(I)+AK3(I)) + AK4(I))/6.0D0
       END DO
+      END SUBROUTINE RK4
 
-      RETURN
-      END
-C
-C 
-      SUBROUTINE RK1(X,H)
+
+      SUBROUTINE RK1(X, H)
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      DIMENSION X(100),TEMP(100),AK1(100),AK2(100),AK3(100)
-      DIMENSION AK4(100),PRIME(100)
-      COMMON/C3/N
+      INTEGER I
+      DOUBLE PRECISION X(100), TEMP(100), AK1(100), AK2(100), AK3(100), AK4(100), PRIME(100)
+      COMMON /C3/ N
 
-      DO I=1,N
-      TEMP(I)=X(I)
+      ! Simple RK4 for base system (first N variables)
+      DO I = 1, N
+          TEMP(I) = X(I)
       END DO
 
-      CALL DER(TEMP,PRIME)
-      DO I=1,N
-      AK1(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK1(I)/2.0D0
+      CALL DER(TEMP, PRIME)
+      DO I = 1, N
+          AK1(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK1(I)/2.0D0
       END DO
 
-      CALL DER(TEMP,PRIME)
-      DO I=1,N
-      AK1(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK1(I)/2.0D0
+      CALL DER(TEMP, PRIME)
+      DO I = 1, N
+          AK2(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK2(I)/2.0D0
       END DO
 
-      CALL DER(TEMP,PRIME)
-      DO I=1,N
-      AK2(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK2(I)/2.0D0
+      CALL DER(TEMP, PRIME)
+      DO I = 1, N
+          AK3(I) = H*PRIME(I)
+          TEMP(I) = X(I) + AK3(I)
       END DO
 
-      CALL DER(TEMP,PRIME)
-      DO I=1,N
-      AK3(I)=H*PRIME(I)
-      TEMP(I)=X(I)+AK3(I)
+      CALL DER(TEMP, PRIME)
+      DO I = 1, N
+          AK4(I) = H*PRIME(I)
+          X(I) = X(I) + (AK1(I) + 2.0D0*(AK2(I)+AK3(I)) + AK4(I))/6.0D0
       END DO
+      END SUBROUTINE RK1
 
-      CALL DER(TEMP,PRIME)
-      DO I=1,N
-      AK4(I)=H*PRIME(I)
-                                                                                
-      X(I)=X(I)+1/6.0D0*(AK1(I)+2.0D0*(AK2(I)+AK3(I))+AK4(I))
-      END DO
 
-      RETURN
-      END
-
-      SUBROUTINE DER(X,PRIME)
+      SUBROUTINE DER(X, PRIME)
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      DIMENSION X(100),PRIME(100)
-        
-	
-	COMMON/C1/ar,ak,abeta,abet0,amu,ags,agi,d,T,eps,beta
-	COMMON/C3/N
-	
-      pi=4*atan(1.0d0)
-       beta = abeta*(1.0d0 + abet0*(sin(0.01d0*T)))
-	
-	!write(*,*)amu,xigma,abeta,agma,at,aep
-	!write(*,*)abeta0,T
-		
-	PRIME(1) = ar*(x(1)+x(2))*(1.0d0-(x(1)+x(2))/ak)
-     & -ags*x(1)*x(3)-beta*x(1)*x(2)
-	PRIME(2) = beta*x(1)*x(2) -agi*x(2)*x(3)-amu*x(2)
-	PRIME(3) = eps*(ags*x(1)+agi*x(2))*x(3)-d*x(3)
+      INTEGER I
+      DOUBLE PRECISION X(100), PRIME(100), pi
+      COMMON /C1/ ar, ak, abeta, abet0, amu, ags, agi, d, T, eps, beta
+      COMMON /C3/ N
 
-      RETURN
-      END
-C
-C
-      SUBROUTINE DERIVE(X,PRIME)
+      ! Compute current transmission β
+      pi   = 4.0D0 * ATAN(1.0D0)
+      beta = abeta * (1.0D0 + abet0 * SIN(0.01D0 * T))
+
+      ! Base dynamics (prey, infected prey, predator)
+      PRIME(1) = ar*(X(1)+X(2))*(1.0D0-(X(1)+X(2))/ak) - ags*X(1)*X(3) - beta*X(1)*X(2)
+      PRIME(2) = beta*X(1)*X(2) - agi*X(2)*X(3) - amu*X(2)
+      PRIME(3) = eps*(ags*X(1)+agi*X(2))*X(3) - d*X(3)
+      END SUBROUTINE DER
+
+
+      SUBROUTINE DERIVE(X, PRIME)
       IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-      DIMENSION X(100),PRIME(100)
-        
-	COMMON/C1/ar,ak,abeta,abet0,amu,ags,agi,d,T,eps,beta
-	COMMON/C3/N
-	
-      pi=4*atan(1.0d0)
-       beta = abeta*(1.0d0 + abet0*(sin(0.01d0*T)))
-	
-	!write(*,*)ar,ak,beta,abet0,amu,ags,agi,d,T,eps
-	!write(*,*)abeta0,T
-		
-	PRIME(1) = ar*(x(1)+x(2))*(1.0d0-(x(1)+x(2))/ak)
-     & -ags*x(1)*x(3)-beta*x(1)*x(2)
-	PRIME(2) = beta*x(1)*x(2) -agi*x(2)*x(3)-amu*x(2)
-	PRIME(3) = eps*(ags*x(1)+agi*x(2))*x(3)-d*x(3)
-      
+      INTEGER I, NN
+      DOUBLE PRECISION X(100), PRIME(100), pi
+      COMMON /C1/ ar, ak, abeta, abet0, amu, ags, agi, d, T, eps, beta
+      COMMON /C3/ N
 
-      DO I= 0,N-1
-      pi=4*atan(1.0d0)
-       beta = abeta*(1.0d0+abet0*(sin(0.01d0*T)))
-	!write(*,*) abeta0
-	
-       PRIME(4+I) = (ar*(1.0d0-((x(1)+x(2))/ak))-ar*((x(1)+x(2))/ak)
-     & -x(3)*ags-x(2)*beta)*x(4+I)+(ar*(1.0d0-((x(1)+x(2))/ak))
-     & -(ar*((x(1)+x(2))/ak))-beta*x(1))*x(7+I)-x(1)*ags*x(10+I) 
-      PRIME(7+I) = x(2)*beta*x(4+I) + (-x(3)*agi-amu+x(1)*beta)*x(7+I)
-     & -x(2)*agi*x(10+I)
-      PRIME(10+I)= x(3)*eps*ags*x(4+I)+eps*x(3)*agi*x(7+I)
-     & +((x(1)*ags+x(2)*agi)*eps-d)*x(10+I)
-    
-	END DO
-      RETURN
-      END
-                                                                      
+      NN = N*N + N     ! Dimension including variational part
+
+      ! Compute current transmission β
+      pi   = 4.0D0 * ATAN(1.0D0)
+      beta = abeta * (1.0D0 + abet0 * SIN(0.01D0 * T))
+
+      ! Base system equations
+      PRIME(1) = ar*(X(1)+X(2))*(1.0D0-(X(1)+X(2))/ak) - ags*X(1)*X(3) - beta*X(1)*X(2)
+      PRIME(2) = beta*X(1)*X(2) - agi*X(2)*X(3) - amu*X(2)
+      PRIME(3) = eps*(ags*X(1)+agi*X(2))*X(3) - d*X(3)
+
+      ! Variational (Jacobian) equations
+      DO I = 0, N-1
+          PRIME(4+I) = (ar*(1.0D0-(X(1)+X(2))/ak) - ar*((X(1)+X(2))/ak)
+     &                   - ags*X(3) - beta*X(2))*X(4+I)
+     &               + (ar*(1.0D0-(X(1)+X(2))/ak) - ar*((X(1)+X(2))/ak)
+     &                   - beta*X(1))*X(7+I)
+     &               - ags*X(1)*X(10+I)
+
+          PRIME(7+I) = beta*X(1)*X(4+I)
+     &               + (-agi*X(3)-amu+beta*X(1))*X(7+I)
+     &               - agi*X(2)*X(10+I)
+
+          PRIME(10+I)= eps*ags*X(3)*X(4+I)
+     &               + eps*agi*X(3)*X(7+I)
+     &               + (eps*(ags*X(1)+agi*X(2)) - d)*X(10+I)
+      END DO
+      END SUBROUTINE DERIVE
